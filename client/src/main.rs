f31b3a6627fd2b17a73df3e7bb3071c8d7f99e2d
sidoux <mirsidahmed@hotmail.fr>
2021-10-17 14:51:08 +0200

    je crois qu e ca marche

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,11 +4,10 @@ use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
 
-const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
 fn main() {
-    let mut client = TcpStream::connect(LOCAL).expect("Failed to create a stream");
+    let mut client = TcpStream::connect("127.0.0.1:6000").expect("Failed to create a stream");
     client.set_nonblocking(true).expect("Failed to initialize non-blocking");
 
     let (tx, rx) = mpsc::channel::<String>();
@@ -17,12 +16,35 @@ fn main() {
         let mut buff = vec![0; MSG_SIZE];
         match client.read_exact(&mut buff) {
             Ok(_) => {
-                let msg = buff.into_iter().take_while(&x x != 0).collect::<Vec<_>>();
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
             },
             Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
             Err(_) => {
                 break;
             }
         }
+
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("writing to socket failed");
+                println!("message sent {:?}", msg);
+            }, 
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break
+        }
+
+        thread::sleep(Duration::from_millis(100));
     });
+
+    println!("Tapez un message (-q pour quitter) :");
+    loop {
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("Failed to read input");
+        let msg = buff.trim().to_string();
+        if msg == "-q" || tx.send(msg).is_err() {break}
+    }
+    println!("Good bye!");
+
 }
